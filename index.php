92fdd6da8b9151fdc331623e96d96776949af1c1
lilian <l.stoklosa@simplon-charleville.fr>
2019-08-05 08:44:24 +0200

    start exo qcm

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>QCM</title>
+    <link rel="stylesheet" href="public/css/index.css">
+  </head>
+  <body>
+
+  </body>
+</html>
