eb1ec4946985f8179574211f2f8d62ae0d25f292
lilian <l.stoklosa@simplon-charleville.fr>
2019-08-09 12:13:00 +0200

    revoir la boucle afichage

--- view/quizz.php
+++ view/quizz.php
@@ -34,12 +34,12 @@
                     echo $result[$j]['question'].'</h2><br>';
                 } else { ?>
                 <div class="form-check">
-                <input type="checkbox" class="form-check-input" name="answer<?php echo $j ?>" value="<?= $result[$j]['valid'] ?>">
+                    <input type="checkbox" class="form-check-input" name="answer<?php echo $j ?>" value="<?= $result[$j]['valid'] ?>">
                     <label class="form-check-label" for="answer<?php echo $j ?>"><?= $result[$j]['reponse'] ?></label>
                 </div>
                 
             <?php }
-            }?>
+            } ?>
             </div>
             <button type="submit" name="endqcm">Valider mes réponses</button>
             </form>
